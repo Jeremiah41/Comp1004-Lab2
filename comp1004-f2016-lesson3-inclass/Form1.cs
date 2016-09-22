d1459e494fcd3aed7c139fe69a90abfe95fcfbe4
Jeremiah41 <jeremiahhughes12@hotmail.ca>
2016-09-22 16:51:03 -0400

    Add project files.

---
+++ comp1004-f2016-lesson3-inclass/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace comp1004_f2016_lesson3_inclass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
